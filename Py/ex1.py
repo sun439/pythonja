d41b77d0235e55d716417179b25ea34c8cc4fce4
Suun439 <mari.sol-439@hotmail.com>
2023-09-04 14:51:38 -0600

    Ejercicios de Python certificación Desarrollo web

---
+++ Py/ex1.py
@@ -0,0 +1,29 @@
+#Instrucciones, pde al usuario nombre y edad y muestra su edad en 10 años
+'''nombre = input("Nombre:  ")
+edad = int(input("Edad:  "))
+edad += 10
+print("Hola ", nombre, ", en 10 años tendras", edad)'''
+
+#Instrucciones 2: Pide la base y la altura de un triangulo y calcula el área
+'''base = float(input("Base del triangulo: "))
+altura = float(input("Altura del triangulo: "))
+area = (base * altura)/ 2
+print("El área es: ", area)'''
+
+#Conectar 2 cadenas utiliando el operador +
+'''nombre = input("Nombre:  ")
+apellido = input("Apellido:  ")
+saludo = "Hola ",nombre +" "+ apellido
+print(saludo)'''
+
+#Creo un array o arreglo de numeros y sumalos, muestra el total
+'''array = [ 1,3,5,7,9 ]
+suma = array[0] + array[1] + array[2] + array[3] + array[4]
+print(suma)'''
+
+#Crea una matriz de 2x2 y muestrala en pantalla
+'''matriz = [[1,2],[3,4]]
+print("Posición (0,0):  ",matriz[0][0])
+print("Posición (0,1):  ",matriz[0][1])
+print("Posición (1,1):  ",matriz[1][1])
+print("Posición (1,0):  ",matriz[1][0])'''
